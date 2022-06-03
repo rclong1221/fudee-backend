6fd6255f776ba2dc12da5a5d71c72a9aafe46bad
dollerinho <dollerinho@Ritches-MacBook-Pro.local>
2022-06-02 21:52:25 -0700

    add package connections

--- config/settings/base.py
+++ config/settings/base.py
@@ -76,6 +76,8 @@ THIRD_PARTY_APPS = [
     "rest_framework.authtoken",
     "corsheaders",
     "drf_spectacular",
+    "phonenumber_field",
+    "django_filters",
 ]
 
 LOCAL_APPS = [
@@ -307,6 +309,7 @@ REST_FRAMEWORK = {
     ),
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 # django-cors-headers - https://github.com/adamchainz/django-cors-headers#setup
