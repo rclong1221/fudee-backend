ad2ecb5d46f90e6396cb6c4e3239c42f9b602cb7
dollerinho <dollerinho@Ritches-MacBook-Pro.local>
2022-06-09 11:49:17 -0700

    create relationship model

--- fudee/relationships/models.py
+++ fudee/relationships/models.py
@@ -26,3 +26,31 @@ class Invite(models.Model):
     def clean(self):
         if not self.email and not self.phone:
             raise ValidationError("Email address or phone number is required.")
+        
+class Relationship(models.Model):
+    uuid = models.UUIDField( # Used by the API to look up the record 
+        db_index=True,
+        unique=True,
+        default=uuid_lib.uuid4,
+        editable=False)
+    
+    user1 = models.ForeignKey(User, related_name="Friend_user1", on_delete=models.PROTECT)
+    user2 = models.ForeignKey(User, related_name="Friend_user2", on_delete=models.PROTECT)
+    relationship = models.IntegerField(validators=[MinValueValidator(-1), MaxValueValidator(2)])
+    date_created = models.DateField(auto_now_add=True, blank=True)
+    date_accepted = models.DateField(blank=True, null=True)
+    date_updated = models.DateField(blank=True, null=True)
+    updater_id = models.IntegerField(blank=True, null=True)
+    
+    def clean(self):
+        if self.user1.id == self.user2.id:
+            raise ValidationError("User cannot have a relationship with themself.")
+        
+        if self.updater_id is None:
+            pass
+        elif self.updater_id != self.user1.id and self.updater_id != self.user2.id:
+            raise ValidationError("Updater ID is invalid.")
+    
+    class Meta:
+        unique_together = (('user1', 'user2'),)
+        index_together = (('user1', 'user2'),)
