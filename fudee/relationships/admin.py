cb0dce85ca7b5a0d22f44dd735e142dcd9a35200
Roux Long <rclong1221@gmail.com>
2022-06-10 09:22:32 -0700

    fix whitespace

--- fudee/relationships/admin.py
+++ fudee/relationships/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from fudee.relationships.models import Invite, Relationship, User_Group
+
 admin.site.register(Invite)
 admin.site.register(Relationship)
 admin.site.register(User_Group)
