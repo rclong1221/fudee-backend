c9a74a158577277ba3cc3c1b6cd492e675faf107
dollerinho <dollerinho@Ritches-MacBook-Pro.local>
2022-06-09 17:07:03 -0700

    hook up relationship model

--- config/api_router.py
+++ config/api_router.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from fudee.users.api.views import UserViewSet
-from fudee.relationships.api.views import InviteViewSet
+from fudee.relationships.api.views import InviteViewSet, RelationshipViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -11,6 +11,7 @@ else:
 
 router.register("users", UserViewSet)
 router.register("invite", InviteViewSet)
+router.register("relationship", RelationshipViewSet)
 
 app_name = "api"
 urlpatterns = router.urls
