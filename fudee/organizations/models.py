06ac746a3120349a2de7edd6274042630c756cb7
Roux Long <rclong1221@gmail.com>
2022-07-09 15:49:20 -0700

    create orguser model

--- fudee/organizations/models.py
+++ fudee/organizations/models.py
@@ -29,3 +29,31 @@ class Organization(models.Model):
     def clean(self):
         if not self.name:
             raise ValidationError("Valid organization name is required.")
+
+class OrganizationUser(models.Model):
+    """
+    
+    """
+    uuid = models.UUIDField( # Used by the API to look up the record 
+        db_index=True,
+        unique=True,
+        default=uuid_lib.uuid4,
+        editable=False)
+    organization = models.ForeignKey(Organization, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    access = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(2)])
+    is_active = models.BooleanField(default=False, blank=False, null=False)
+    date_created = models.DateField(auto_now_add=True, blank=True)
+    date_accepted = models.DateField(blank=True, null=True)
+    date_updated = models.DateField(blank=True, null=True)
+    updater_id = models.IntegerField(blank=True, null=True)
+    
+    def clean(self):
+        if self.updater_id is None:
+            pass
+        elif self.updater_id != self.user.id:
+            raise ValidationError("Updater ID is invalid.")
+    
+    class Meta:
+        unique_together = (('organization', 'user'),)
+        index_together = (('organization', 'user'),)
