2ee70e963f0bf70b05c035c3b187b3f4b626c06c
Roux Long <rclong1221@gmail.com>
2022-07-13 14:19:27 -0700

    add eventimage to admin

--- fudee/events/admin.py
+++ fudee/events/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from fudee.events.models import Event, EventUser
+from fudee.events.models import Event, EventUser, EventImage
 
 admin.site.register(Event)
 admin.site.register(EventUser)
+admin.site.register(EventImage)
