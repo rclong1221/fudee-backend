21a6f347fbc8c67ceeb87825de08d40e896d7673
Roux Long <rclong1221@gmail.com>
2022-07-13 13:48:04 -0700

    create eventuser serializers

--- fudee/events/api/serializers.py
+++ fudee/events/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from fudee.events.models import Event
+from fudee.events.models import Event, EventUser
 
 from datetime import datetime
 
@@ -55,3 +55,52 @@ class CreateEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ["uuid", "title", "description", "location", "recurrences", "date_start", "date_end", "user", "date_updated", "updater_id"]
+
+class GetEventUserSerializer(serializers.ModelSerializer):
+    uuid = serializers.UUIDField(format="hex_verbose", read_only=True)
+    access = serializers.IntegerField(read_only=True)
+    is_active = serializers.BooleanField(read_only=True)
+    date_accepted = serializers.DateField(read_only=True)
+    date_updated = serializers.DateField(read_only=True)
+    updater_id = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = EventUser
+        fields = ["uuid", "event", "user", "access", "is_active", "date_created", "date_accepted", "date_updated", "updater_id"]
+
+class CreateEventUserSerializer(serializers.ModelSerializer):
+    uuid = serializers.UUIDField(format="hex_verbose", read_only=True)
+    event = serializers.IntegerField()
+    user = serializers.IntegerField()
+    access = serializers.IntegerField()
+    is_active = serializers.BooleanField()
+    date_accepted = serializers.DateField(read_only=True)
+    date_updated = serializers.DateField(read_only=True)
+    updater_id = serializers.IntegerField()
+    
+    def create(self, validated_data):
+        """
+        """
+        user = User.objects.get(id=validated_data['user'])
+        event = Event.objects.get(id=validated_data['event'])
+        data = EventUser.objects.create(
+            event=event,
+            user=user,
+            access=validated_data['access'],
+            is_active=validated_data['is_active'],
+            date_updated=datetime.now().strftime("%Y-%m-%d"),
+            updater_id=validated_data['updater_id'],
+        )
+        return data
+
+    def update(self, instance, validated_data):
+        # instance.access = validated_data.get('access', instance.access)
+        instance.is_active = validated_data.get('is_active', instance.is_active)
+        instance.date_updated = datetime.now().strftime("%Y-%m-%d")
+        instance.updater_id = validated_data.get('updater_id', instance.updater_id)
+        instance.save()
+        return instance
+    
+    class Meta:
+        model = EventUser
+        fields = ["uuid", "event", "user", "access", "is_active", "date_created", "date_accepted", "date_updated", "updater_id"]
