e3b225b3fbfa9c40e9dcd8c6f2c25e4eaff0ed18
Roux Long <rclong1221@gmail.com>
2022-07-21 13:24:32 -0700

    add isorganizationuser permission

---
+++ fudee/organizations/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+from collections.abc import Iterable
+
+class IsOrganizationUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if obj.user != request.user:
+            return False
+        if request.method == 'PUT' or request.method == 'PATCH' or request.method == 'DELETE':
+            if obj.access < 2:
+                return False
+        if obj.access < 1:
+            return False
+        return True
