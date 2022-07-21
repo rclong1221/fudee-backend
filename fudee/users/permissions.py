85b4119e7a0bbf7132fbc3983e2ae0f9fbd8ad73
Roux Long <rclong1221@gmail.com>
2022-07-20 23:49:45 -0700

    create userimage permission

--- fudee/users/permissions.py
+++ fudee/users/permissions.py
@@ -10,3 +10,15 @@ class IsUserOwner(permissions.BasePermission):
         if obj == request.user:
             return True
         return False
+
+class IsUserImageOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_authenticated:
+            return True
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if obj.user == request.user:
+            return True
+        
+        return False
