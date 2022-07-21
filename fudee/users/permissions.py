4e5a3c2b74739b224b4326cfa2ba0c201593a687
Roux Long <rclong1221@gmail.com>
2022-07-20 22:44:36 -0700

    change class name

--- fudee/users/permissions.py
+++ fudee/users/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
+class IsUserOwner(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.user.is_authenticated:
             return True
